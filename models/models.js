9da53eadfc7a26340bf05fd58fe794855064b2aa
root <root@Node.js>
2015-06-11 20:25:43 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -1,40 +1,43 @@
 var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/); 
-var DB_name     = (url[6]||null);
-var user        = (url[2]||null);
-var pwd         = (url[3]||null);
-var protocol    = (url[1]||null);
-var dialect     = (url[1]||null);
-var port        = (url[5]||null);
-var host        = (url[4]||null);
-var storage     = process.env.DATABASE_STORAGE;
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6]||null);
+var user      = (url[2]||null);
+var pwd       = (url[3]||null);
+var protocol  = (url[1]||null);
+var dialect   = (url[1]||null);
+var port      = (url[5]||null);
+var host      = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
-  { dialect: protocol,
-    protocol: protocol,
-    port: port,
-    host: host,
-    storage: storage, // solo SQLite (.env)
-    omitNull: true // solo Postgres
+{ dialect: protocol,
+  protocol: protocol,
+  port: port,
+  host: host,
+  storage: storage, // solo SQLite (.env)
+  omitNull: true // solo Postgres
   }
 );
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
+
 exports.Quiz = Quiz; // exportar tabla Quiz
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-  // then(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().then(function (count){
-      if(count === 0) { // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
-                  })
-    .then(function(){console.log('Base de datos inicializada')});
+// then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+    if(count === 0) { // la tabla se inicializa solo si está vacía
+      Quiz.bulkCreate(
+        [ {pregunta: 'Capital de Italia', respuesta: 'Roma'},
+        {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var auth = require('../controllers/author_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+	res.render('index', { title: 'Quiz' });
 });
 router.get('/author',function(req,res){
-	res.render('author', { 
-				fotoPerfil: '/images/foto.png', 
-				Nombre: 'J.Rafa RN', 
+	res.render('author', {
+				fotoPerfil: '/images/foto.png',
+				Nombre: 'J.Rafa RN',
 				Video: 'http://vjs.zencdn.net/v/oceans.mp4'
-			});
-});	
+	});
+});
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
 
