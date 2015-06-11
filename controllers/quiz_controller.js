c4eb79217f62961648431073cb421aef5cd9081e
root <root@Node.js>
2015-06-11 19:29:51 +0200

    Despliegue Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 var models = require('../models/models.js');
 
 exports.question = function(req, res) {
-	models.Quiz.findAll().success(function(quiz){
+	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/question', {pregunta: quiz[0].pregunta});		
 	})
 };
 
 exports.answer = function(req, res) {
-	models.Quiz.findAll().success(function(quiz){
+	models.Quiz.findAll().then(function(quiz){
 		if (req.query.respuesta === quiz[0].respuesta){
 			res.render('quizes/answer', {respuesta: 'Correcto'});
 		} else {
