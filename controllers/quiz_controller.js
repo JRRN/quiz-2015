ed230e3bbee08332120551404634208dda522fa6
root <root@Node.js>
2015-06-14 12:00:35 +0200

    Borrar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -105,3 +105,9 @@ exports.update = function(req, res) {
 	}
  );
 };
+
+exports.destroy = function(req, res) {
+ req.quiz.destroy().then( function() {
+	 res.redirect('/quizes');
+ }).catch(function(error){ next(error) });
+};
--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ router.get('/quizes/new'					, quizController.new);
 router.post('/quizes/create'				, quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit'		, quizController.edit);
 router.put('/quizes/:quizId(\\d+)'			, quizController.update);
+router.delete('/quizes/:quizId(\\d+)'		, quizController.destroy);
 
 module.exports = router;
 
